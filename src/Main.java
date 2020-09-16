cfaa3b13c39c6944287420f01f7217421a1fc40a
eugenebondarchuk <bondarchuk.eugene@gmial.com>
2020-09-16 21:45:20 +0300

    amend logic for count tasks solved

--- src/Main.java
+++ src/Main.java
@@ -3,32 +3,52 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-       int numberofTask = getRandomNumberofTasks();
-       Task[] tasksArray = new Task[numberofTask];
-       tasksArray = createTasklist(numberofTask);
-       printTaskarray(tasksArray);
-       Mentor mentorOne = new Mentor("Vasya","Ivanob",35);
-       Student studentOne = new Student("Jane","Petrova",23,0,mentorOne,false);
-       studentOne.solveTasks(tasksArray);
-       System.out.println(studentOne.getPassedTasksCount());
+        int numberofTask = getRandomNumberofTasks();
+        Task[] tasksArray = new Task[numberofTask];
+        tasksArray = createTasklist(numberofTask);
+        printTaskarray(tasksArray);
+        Mentor mentorOne = new Mentor("Vasya", "Ivanob", 35);
+        Student studentOne = new Student("Jane", "Petrova", 23, 0, mentorOne, false);
+        Student studentTwo = new Student("George", "Sidorov", 25, 0, mentorOne, false);
+
+        Student[] studentsArray = {studentOne, studentTwo};
+
+        int tasksToBeSolvedByOneStudent = 8;
+
+        while (Student.getAllStudentsTasksCount() <= numberofTask) {
+            for (Student obj : studentsArray
+            ) {
+                obj.solveTasks(tasksArray);
+                if (obj.getPassedTasksCount() >= tasksToBeSolvedByOneStudent) {
+                    continue;
+                }
+
+            }
+        }
+
+//       studentOne.solveTasks(tasksArray);
+        System.out.println(studentOne.getPassedTasksCount());
+        System.out.println(studentTwo.getPassedTasksCount());
+        System.out.println(Student.getAllStudentsTasksCount());
+        System.out.println(Student.totalAttempts);
     }
 
-    public static Task[] createTasklist (int numberOfTasks){
+    public static Task[] createTasklist(int numberOfTasks) {
 
         Task[] tasksArray = new Task[numberOfTasks];
         Task obj = null;
         Random randomizer = new Random();
-        for (int i=0; i< tasksArray.length; i++) {
+        for (int i = 0; i < tasksArray.length; i++) {
             int randomNumber = randomizer.nextInt(3);
-            switch(randomNumber) {
+            switch (randomNumber) {
                 case 0:
-                    obj = new Test(i,"Test text");
+                    obj = new Test(i, "Test text");
                     break;
                 case 1:
-                    obj = new DragAndDrop(i,"Some task text");
+                    obj = new DragAndDrop(i, "Some task text");
                     break;
                 case 2:
-                    obj = new Code(i,"Some task text");
+                    obj = new Code(i, "Some task text");
                     break;
             }
             tasksArray[i] = obj;
@@ -36,15 +56,15 @@ public class Main {
         return tasksArray;
     }
 
-    public static void printTaskarray (Task[] tasksArray){
-        for (Task obj:tasksArray
-             ) {
+    public static void printTaskarray(Task[] tasksArray) {
+        for (Task obj : tasksArray
+        ) {
             System.out.println(obj);
 
         }
     }
 
-    public static int getRandomNumberofTasks (){
+    public static int getRandomNumberofTasks() {
         Random randomizer = new Random();
         int randomNumber = randomizer.nextInt(30);
         return randomNumber;
--- src/Student.java
+++ src/Student.java
@@ -4,6 +4,7 @@ public class Student extends Person {
 
     private int passedTasksCount;
     private static int allStudentsTasksCount;
+    public static int totalAttempts =0;
     private Mentor mentor;
     private boolean areAllTaskspassed;
 
@@ -15,24 +16,33 @@ public class Student extends Person {
         this.areAllTaskspassed = areAllTaskspassed;
     }
 
-        public void solveTasks (Task [] tasksList){
+    public void solveTasks(Task[] tasksList) {
 
         for (int i = this.passedTasksCount; i < tasksList.length; i++) {
             this.solveTask(tasksList[i]);
         }
     }
 
-    private void solveTask (Task task){
-        if (task instanceof Autochecked){
-            System.out.println("Task solved");
-            this.passedTasksCount +=1;
+    private void solveTask(Task task) {
+        if (task instanceof Autochecked) {
+            System.out.println("Task solved by " + this.name + " " + this.surname);
+            this.passedTasksCount += 1;
+            Student.allStudentsTasksCount += 1;
+            totalAttempts+=1;
+        } else {
+            Mentor mentor = new Mentor("Вася", "Ivanov", 25);
+            boolean isMentorHelp = false;
+            while (mentor.makeCodeReview(task)) {
+                totalAttempts+=1;
+            }
+            this.passedTasksCount+=1;
+            Student.allStudentsTasksCount += 1;
+            totalAttempts+=1;
+
+              ;
+            }
         }
-        else {
-            Mentor mentor = new Mentor("Вася","Ivanov",25);
-            while (mentor.makeCodeReview(task))
-            this.passedTasksCount +=1;
-        }
-    }
+
 
 
     public int getPassedTasksCount() {
