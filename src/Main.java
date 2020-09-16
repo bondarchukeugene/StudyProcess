440376d870174dd882935fbed452ad7b02040e98
eugenebondarchuk <bondarchuk.eugene@gmial.com>
2020-09-16 19:21:32 +0300

    add method to get random number of tasks

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,19 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-       Task[] tasksArray = new Task[30];
-       tasksArray = createTasklist();
+       int numberofTask = getRandomNumberofTasks();
+       Task[] tasksArray = new Task[numberofTask];
+       tasksArray = createTasklist(numberofTask);
        printTaskarray(tasksArray);
        Mentor mentorOne = new Mentor("Vasya","Ivanob",35);
        Student studentOne = new Student("Jane","Petrova",23,0,mentorOne,false);
        studentOne.solveTasks(tasksArray);
-        System.out.println(studentOne.getPassedTasksCount());
+       System.out.println(studentOne.getPassedTasksCount());
     }
 
-    public static Task[] createTasklist (){
+    public static Task[] createTasklist (int numberOfTasks){
 
-        Task[] tasksArray = new Task[30];
+        Task[] tasksArray = new Task[numberOfTasks];
         Task obj = null;
         Random randomizer = new Random();
         for (int i=0; i< tasksArray.length; i++) {
@@ -41,6 +42,12 @@ public class Main {
             System.out.println(obj);
 
         }
+    }
+
+    public static int getRandomNumberofTasks (){
+        Random randomizer = new Random();
+        int randomNumber = randomizer.nextInt(30);
+        return randomNumber;
     }
 
 
